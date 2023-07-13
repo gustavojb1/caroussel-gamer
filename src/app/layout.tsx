f650274c6488ebde6f7f2f022cd67806306c635a
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-07-13 12:34:10 -0300

    header and initial styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,50 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import "./globals.scss"
+import Image from "next/image";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
-  title: 'Caroussel Gamer',
-  description: 'Created by Gustavo Junqueira Barbosa',
-}
+  title: "Caroussel Gamer",
+  description: "Created by Gustavo Junqueira Barbosa",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >
+        <header>
+          <Image
+            src="/icons/menu.svg"
+            alt="Menu options"
+            width={36}
+            height={25}
+            priority
+          />
+          <Link href="/">
+            <Image
+              src="/spider-logo.svg"
+              alt="Spiderman"
+              width={260}
+              height={70}
+              priority
+            />
+          </Link>
+          <Image
+            src="/icons/user.svg"
+            alt="Login"
+            width={36}
+            height={36}
+            priority
+          />
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
