45dec60bed2a3cbcd62e6da3706bcbce798d17de
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-07-19 19:29:53 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { IHeroData } from "@/interfaces/heroes";
 
 async function getData(): Promise<{ data: IHeroData[] }> {
   const res = await fetch(
-    "http://caroussel-gamer.vercel.app/api/heroes"
+    "https://caroussel-gamer-gustavojb1.vercel.app//api/heroes"
   );
 
   if (!res.ok) {
